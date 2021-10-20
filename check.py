bdd596b43db8bb9df0bf8f88c8ab6f1c92f53864
Dimlitter <68945228+Dimlitter@users.noreply.github.com>
2021-10-20 12:19:05 +0800

    Update check.py

--- check.py
+++ check.py
@@ -6,9 +6,8 @@ import time
 import os
 import random
 from requests import post
-TG_TOKEN: ${{ secrets.TG_TOKEN }}	#TG机器人的TOKEN
-CHAT_ID: ${{ secrets.CHAT_ID }}	#推送消息的CHAT_ID
-
+TG_TOKEN = secrets.TG_TOKEN #TG机器人的TOKEN
+CHAT_ID = secrets.CHAT_ID	#推送消息的CHAT_ID
 
 class LoginError(Exception):
     """Login Exception"""
@@ -299,8 +298,8 @@ if __name__ == '__main__':
     f_name = "account.json"
     # 填写要自动打卡的：账号 密码, 然后自己实现循环即可帮多人打卡
     # aps = [("<username>", "<password>")]
-    account: ${{ secrets.account }}
-    pwd: ${{ secrets.pwd }}
+    account= secrets.account
+    pwd= secrets.pwd
     if account == "" or pwd == "":
         if not os.path.exists(f_name):
             with open(f_name, "w") as f:
