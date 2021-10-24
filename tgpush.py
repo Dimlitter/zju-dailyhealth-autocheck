f6de89fb177d1163dcd687a87ea5acfde99e073d
Dimlitter <68945228+Dimlitter@users.noreply.github.com>
2021-10-24 12:54:39 +0800

    Create tgpush.py

---
+++ tgpush.py
@@ -0,0 +1,29 @@
+from requests import post
+
+"""
+TG 消息推送模块
+"""
+
+TG_TOKEN = '1336324972:AAHlbuz5UtG-3pzJNHp17BxvBAzNDQyDk0M'	#TG机器人的TOKEN
+CHAT_ID = '1055165212'	#推送消息的CHAT_ID
+
+
+def post_tg(message):
+    telegram_message = f"{message}"
+               
+    params = (
+        ('chat_id', CHAT_ID),
+        ('text', telegram_message),
+        ('parse_mode', "Markdown"), #可选Html或Markdown
+        ('disable_web_page_preview', "yes")
+    )    
+    telegram_url = "https://api.telegram.org/bot" + TG_TOKEN + "/sendMessage"
+    telegram_req = post(telegram_url, params=params)
+    telegram_status = telegram_req.status_code
+    if telegram_status == 200:
+        print(f"INFO: Telegram Message sent")
+    else:
+        print("Telegram Error")
+        
+if __name__ == "__main__":
+    post_tg('浙江大学每日健康打卡 V1.0 '+ " \n\n 签到结果: " + res.get("m"))    
