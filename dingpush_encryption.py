1d4f69754deb52f9dea706537ce566e202fefe64
Dimlitter <68945228+Dimlitter@users.noreply.github.com>
2021-11-14 20:52:46 +0800

    有空再修 欢迎pr

--- dingpush_encryption.py
+++ dingpush_encryption.py
@@ -5,7 +5,10 @@ import time
 import json
 import hashlib
 import base64
-   
+
+DD_BOT_TOKEN = os.getenv("DD_BOT_TOKEN")
+DD_BOT_SECRET=os.getenv("DD_BOT_SECRET") #哈希算法验证(可选)
+  
 def dingding_bot(title, content):
     timestamp = str(round(time.time() * 1000))  # 时间戳
     secret_enc = DD_BOT_SECRET.encode('utf-8')
@@ -25,15 +28,31 @@ def dingding_bot(title, content):
         print('推送成功！')
     else:
         print('推送失败！')
-        
-        
-def main():
-    send('title', 'content')
+         
+def post_ding(url,reminders,msg):
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    data = {
+    "msgtype": "text",
+    "text": {
+        "content": msg
+    },
+    "at": {
+        "atMobiles": [   #此处为需要@什么人
+    
+        ],
+        "isAtAll": False
+    }
+}
+    r = requests.post(url,data=json.dumps(data),headers=headers)
+    return r.text
 
 if __name__ == '__main__':
-    title = "测试"
-    content = "钉钉密钥方式推送"
-    DD_BOT_TOKEN=""
-    DD_BOT_SECRET=""#哈希算法验证
-    dingding_bot(title,content)
-    
+    if DD_BOT_SECRET == "" : 
+        msg = "测试内容" 
+        reminders = ['']
+        url = f'https://oapi.dingtalk.com/robot/send?access_token={DD_BOT_TOKEN}' 
+        print(post_ding(url, reminders, msg))
+    else :
+        title = "浙江大学健康打卡"
+        content = "钉钉密钥方式推送"
+        dingding_bot(title,content)
